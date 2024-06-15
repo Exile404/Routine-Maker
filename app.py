9f2de8c3208acedac7a589e706fe8c1268ee0c0f
Exile404 <tahsinul.haque.dhrubo@gmail.com>
2024-06-16 03:54:23 +0600

    modified

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from Backend.mystery import Routine
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import uvicorn
-
+import unicorn
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -33,4 +32,4 @@ async def process_data(request: Request):
     return templates.TemplateResponse("results.html", {"request": request, "result": result})
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
